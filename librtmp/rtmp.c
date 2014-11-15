ad70c648a4115adc9fdd3ede13a1fe85ba178c05
atvcaptain <captain@web.de>
2014-11-15 22:41:11 +0100

    add movecast thx testi

--- librtmp/rtmp.c
+++ librtmp/rtmp.c
@@ -3270,6 +3270,11 @@ HandleInvoke(RTMP *r, const char *body, unsigned int nBodySize)
               SendCommand(r, "goVideStambolSoseBardovci", TRUE);
               RTMP_SendCreateStream(r);
             }
+         else if (strstr(pageUrl, "movecast.tv"))
+            {
+              SendCommand(r, "trebaDaSmenamSecurity", TRUE);
+              RTMP_SendCreateStream(r);
+            }
           else if ((strstr(host, "highwebmedia.com") || strstr(pageUrl, "chaturbate.com"))
                    && (!strstr(host, "origin")))
             {
